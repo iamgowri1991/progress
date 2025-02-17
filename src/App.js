58a3d929891b0b2b1102df73338db227a45d0401
gs001 <gowri_s@intuit.com>
2025-02-17 11:03:50 +0530

    fix: autocomplete, debounce and cache

--- src/App.js
+++ src/App.js
@@ -1,10 +1,66 @@
-import React from "react";
+import React ,{useEffect, useState} from 'react'
+import "./App.css"
 
 export default function App() {
 
+  const [result, setResult] = useState([])
+  const [searchKeyword,setSearchKeyword]= useState("")
+  const [showResult,setShowResults] = useState(false);
+  const [cache,setCache] = useState({})
+
+  const fetchData = async () => {
+    if(cache[searchKeyword])
+    {
+      console.log('Cache HIT',searchKeyword)
+      setResult(cache[searchKeyword])
+      return;
+      
+    }
+    const response = await fetch(`https://dummyjson.com/recipes/search?q=${searchKeyword}`);
+    const data = await response.json();
+    setResult(data?.recipes)
+    setCache({
+      ...cache,
+      [searchKeyword]: data?.recipes,
+    })
+
+  }
+  useEffect(()=>{
+    const timer = setTimeout(fetchData,300)
+    return ()=> clearTimeout(timer);
+   
+  },[searchKeyword])
+
+
+  const handleSeachInput = (e) => {
+    const { value} = e.target
+    setSearchKeyword(value)
+  }
+
+
   return (
-    <>
-      TEST
-    </>
-  );
+    <div style={{textAlign:'center'}}>
+    <h1> Auto complete </h1>
+    <div>
+      <input  
+        onFocus={()=>setShowResults(true)}  
+          onBlur={()=>setShowResults(false)}   
+          onChange={handleSeachInput} 
+          type="text" id="seach" style={{width:'500px'}}/>
+      {showResult && (<div className='result-container'>
+        {result.map((eachResult)=> (
+          <span  
+         
+          className="result" key={eachResult.id}>
+          {eachResult.name}
+          </span>  
+        ))}
+      </div>)
+      }
+      
+    </div>
+      
+    </div>
+  )
 }
+
